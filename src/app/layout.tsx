8440be124bcb3df3aeda2a08c302555d3f05ec9b
mooowu <me@mooowu.xyz>
2025-08-15 22:04:36 +0900

    feat: footer 하단 고정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,11 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
         <GraphQLProvider>
           <Header />
-          {children}
+          <main className="flex-1">{children}</main>
           <Footer />
         </GraphQLProvider>
       </body>
